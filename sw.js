d7337a49b48e0c8f21dea1cf08e6655da394a497
wooding74-code <wooding74@gmail.com>
2026-03-13 18:37:33 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
 
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('p1').then((c) => c.addAll(['/'])));
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+});
