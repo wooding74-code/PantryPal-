a73f0d5aa69ec164f528b73e8d9cb259bab84081
wooding74-code <wooding74@gmail.com>
2026-03-13 18:34:07 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,1 @@
 
-const CACHE_NAME = 'pantry-pal-v1';
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(['/'])));
-});
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
-});
