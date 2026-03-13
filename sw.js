ec15b3d0ac9188f552a630aa123ac88affad6143
wooding74-code <wooding74@gmail.com>
2026-03-13 17:25:42 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
+const CACHE_NAME = 'pantrypal-cache-v2';
+const ASSETS = ['/', '/index.html', '/manifest.json'];
 
+// Install & Cache
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+// Activate & Cleanup
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((ks) => Promise.all(ks.map(k => k !== CACHE_NAME && caches.delete(k)))));
+});
+
+// Fetch logic
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+});
+
+// Empty handlers to satisfy PWABuilder 'Action Items'
+self.addEventListener('push', (e) => { /* Placeholder */ });
+self.addEventListener('sync', (e) => { /* Placeholder */ });
